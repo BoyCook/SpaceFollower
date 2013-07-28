12728fa32af185389b9b0d1655016c14f1f8c211
Craig Cook <boycook@me.com>
2013-07-28 12:16:20 +0100

    Added load script

---
+++ load.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tsapp push spacefollower index.html
+tsapp push spacefollower Elusive-Icons.eot
+tsapp push spacefollower Elusive-Icons.svg
+tsapp push spacefollower Elusive-Icons.ttf
+tsapp push spacefollower Elusive-Icons.woff
+tsapp push spacefollower elusive-webfont.css
+tsapp push spacefollower SpaceFollower.css
+tsapp push spacefollower SpaceFollower.js
